e885dbcc6292765471ae0adaecbb4333b7b69e71
cleitonts <cleitonsorrilha@gmail.com>
2023-04-10 15:20:49 +0100

    avatar management

---
+++ backend/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '35938206b96bf7a69d6482ff038d931b',
+    expiresIn: '1d',
+  },
+};
---
+++ backend/src/modules/users/services/UpdateUserAvatarService.ts
@@ -0,0 +1,38 @@
+import { getCustomRepository } from 'typeorm';
+import AppError from '@shared/errors/AppError';
+import User from '../typeorm/entities/User';
+import UserRepository from '../typeorm/repositories/UserRepository';
+import path from 'path';
+import uploadConfig from '@config/upload';
+import fs from 'fs';
+
+interface IRequest {
+  user_id: string;
+  avatarFilename: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UserRepository);
+    const user = await usersRepository.findById(user_id);
+    if (!user) {
+      throw new AppError('User not found.');
+    }
+
+    if (user.avatar) {
+      const userAvatarFilepath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilepath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilepath);
+      }
+    }
+
+    user.avatar = avatarFilename;
+
+    await usersRepository.save(user);
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
